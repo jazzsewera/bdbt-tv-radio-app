d1575098902b8288dfee9819bf3dea63059dee57
Blazej Sewera <blazejok1@wp.pl>
2021-05-30 23:24:31 +0200

    feat(frontend): migrate from web-test-runner to jest.js

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { expect } from 'chai';
 import { cleanup, Matcher, render, RenderResult, SelectorMatcherOptions } from '@testing-library/react';
 import App from './App';
 
@@ -15,19 +14,19 @@ describe('App tests', () => {
 
   it('renders navbar', () => {
     const navbar = app.getByRole('navigation');
-    expect(navbar.textContent).to.be.equal('HomeLogin');
+    expect(navbar.textContent).toEqual('HomeLogin');
   });
 
   it('clicks on home', () => {
     const homeButton = app.getByText('Home');
     homeButton.click();
-    expect(homeButton.classList.toString).to.contain('navbarBold');
-    expect(homeButton.style.fontWeight).to.be.equal('bold');
+    expect(homeButton.classList.toString).toContain('navbarBold');
+    expect(homeButton.style.fontWeight).toEqual('bold');
   });
 
   it('clicks on login', () => {
     const loginButton = app.getByText('Login');
     loginButton.click();
-    expect(loginButton.style.fontWeight).to.be.equal('bold');
+    expect(loginButton.style.fontWeight).toEqual('bold');
   });
 });
